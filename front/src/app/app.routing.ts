642c55592e330d665a5edce7a6257c1f4dc1734d
munhozfontana <munhozfontana>
2018-09-02 12:08:49 -0300

    correção de lazy load

--- front/src/app/app.routing.ts
+++ front/src/app/app.routing.ts
@@ -1,20 +1,20 @@
 
-import { CarroComponent } from './carro/carro.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
 import { AluguelComponent } from './aluguel/aluguel.component';
-import { HomeComponent } from './home/home.component';
-import { ClienteComponent } from './cliente/cliente.component';
-import { FuncionarioComponent } from './funcionario/funcionario.component';
+
+
 
 const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'aluguel', component: AluguelComponent},
-  { path: 'carro', component: CarroComponent},
-  { path: 'cliente', component: ClienteComponent},
-  { path: 'funcionario', component: FuncionarioComponent}
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  },
+  { path: 'aluguel', component: AluguelComponent },
+  { path: 'carro', loadChildren: 'app/carro/carro.module#CarroModule' },
+  { path: 'cliente',  loadChildren: 'app/cliente/cliente.module#ClienteModule' },
+  { path: 'funcionario',  loadChildren: 'app/funcionario/funcionario.module#FuncionarioModule'}
 ];
 
 
